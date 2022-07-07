770892b1973043ed122c250bc973ee5365cea00d
Do Tuan Huy <dotuanhuy1302@gmail.com>
2022-07-07 13:19:30 +0700

    fix ui

---
+++ src/redux/store.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { createStore } from 'redux';
+const store = createStore() => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default store;
